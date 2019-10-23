c389b450358397fc63e5c7765bf5a17c17faf7eb
zms1999 <zms_thu18@163.com>
2019-10-23 14:48:43 +0800

    modify

--- test/graph_mining_test.cpp
+++ test/graph_mining_test.cpp
@@ -2,11 +2,9 @@
 #include <../include/graph.h>
 
 TEST(graph_mining_test, test_triangle_counting) {
+    Graph g;
+    g.load_patent();
 
-		Graph g;
-		g.load_patent();
-		
-		ASSERT_EQ(g.triangle_counting(), 7515023);
-		
+    ASSERT_EQ(g.triangle_counting(), 7515023);
 }
 
