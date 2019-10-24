0fa845e216200f593aa9f5c3ff929a5e7d1ef4f7
zms1999 <zms_thu18@163.com>
2019-10-24 20:36:39 +0800

    add dataloader

---
+++ include/dataloader.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "./graph.h"
+#include <map>
+#include <algorithm>
+
+
+enum DataType {
+    Patents
+};
+
+class DataLoader {
+public:
+
+    bool load_data(Graph* &g, DataType type, const char* path);
+
+private:
+    static bool cmp_pair(std::pair<int,int>a, std::pair<int,int>b);
+    std::map<int,int> id;
+};
