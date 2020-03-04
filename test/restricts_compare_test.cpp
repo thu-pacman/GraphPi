576222700b2cc4caf26018962b7023fd48be4669
zms1999 <zms_thu18@163.com>
2020-03-04 20:38:44 +0800

    add restrict_generation function, that is performance modeling for restricts

---
+++ test/restricts_compare_test.cpp
@@ -0,0 +1,88 @@
+#include <gtest/gtest.h>
+#include <../include/graph.h>
+#include <../include/dataloader.h>
+#include "../include/pattern.h"
+#include "../include/schedule.h"
+#include "../include/common.h"
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+void test_pattern(Graph* g, PatternType type) {
+    Pattern pattern(type);
+    
+    printf("test pattern : ");
+    PatternType_printer(type);
+
+    bool is_pattern_valid;
+    int performance_modeling_type;
+    
+    performance_modeling_type = 2;
+    Schedule schedule_our(pattern, is_pattern_valid, performance_modeling_type, g->v_cnt, g->e_cnt);
+    ASSERT_EQ(is_pattern_valid, true);
+
+    performance_modeling_type = 2;
+    Schedule schedule_gz(pattern, is_pattern_valid, performance_modeling_type, g->v_cnt, g->e_cnt);
+    ASSERT_EQ(is_pattern_valid, true);
+
+    std::vector< std::pair<int,int> > gz_pairs;
+    schedule_gz.GraphZero_aggressive_optimize(gz_pairs);
+    schedule_gz.add_restrict(gz_pairs);
+
+    std::vector< std::pair<int,int> > our_pairs;
+    schedule_our.restrict_generation(g->v_cnt, g->e_cnt, our_pairs);
+    schedule_our.add_restrict(our_pairs);
+
+    int thread_num = 24;
+    double t1,t2,t3,t4;
+
+    t1 = get_wall_time();
+    long long ans_our = g->pattern_matching(schedule_our, thread_num);
+    t2 = get_wall_time();
+
+    t3 = get_wall_time();
+    long long ans_gz = g->pattern_matching(schedule_gz, thread_num);
+    t4 = get_wall_time();
+
+    printf("our ans: %lld time: %.6lf\n", ans_our, t2 - t1);
+    schedule_our.print_schedule();
+    for(int i = 0; i < our_pairs.size(); ++i)
+        printf("(%d,%d)",our_pairs[i].first, our_pairs[i].second);
+    puts("");
+
+    printf("GZ  ans: %lld time: %.6lf\n", ans_gz, t4 - t3);
+    schedule_gz.print_schedule();
+    for(int i = 0; i < gz_pairs.size(); ++i)
+        printf("(%d,%d)",gz_pairs[i].first, gz_pairs[i].second);
+    puts("");
+}
+
+TEST(restricts_compare_test, restricts_compare_patents) {
+    Graph *g;
+    DataLoader D;
+    
+    std::string type = "Patents";
+    std::string path = "/home/zms/patents_input";
+    DataType my_type;
+    if(type == "Patents") my_type = DataType::Patents;
+    else {
+        printf("invalid DataType!\n");
+    }
+
+    Pattern pattern(PatternType::House);
+
+    ASSERT_EQ(D.load_data(g,my_type,path.c_str()),true); 
+    
+    printf("Load data success!\n");
+    fflush(stdout);
+
+    test_pattern(g, PatternType::Rectangle);
+    test_pattern(g, PatternType::Pentagon);
+    test_pattern(g, PatternType::House);
+    test_pattern(g, PatternType::Hourglass);
+    test_pattern(g, PatternType::Cycle_6_Tri);
+    test_pattern(g, PatternType::Clique_7_Minus);
+
+    delete g;
+}
